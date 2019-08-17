9454ce46ee276544c09b68f0f461c3b05d0d8162
David Kim <dkim437@bloomberg.net>
2019-08-17 18:45:10 -0400

    Send notifications to subscribers

--- functions/index.js
+++ functions/index.js
@@ -2,28 +2,88 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Listen for any change in collection `users`.
-exports.handleEventUpdate = functions.firestore
-  .document('users/{user}').onWrite((change, context) => {
-      console.log("Received change to users collection");
-      const previousValue = change.before.data();
-      const nextValue = change.after.data();
-      console.log("user = " + context.params.user);
-      console.log(previousValue);
-      console.log(nextValue);
+// Get Firestore database handle
+const db = admin.firestore();
 
-      return;
-    });
+// Firebase Cloud Messaging
+async function sendNotification(token, message)
+{
+  const payload = {
+    notification :{
+      title: 'Event Update',
+      body: message,
+    }
+  };
+  const response = await admin.messaging().sendToDevice(token, payload);
 
-// Listen for any change in collection 'events'.
-exports.sendNotification = functions.firestore
-  .document("events/{evt}").onWrite((change, context) => {
-      console.log("Received change to events collection.");
-      const evt = context.params.evt;
-      console.log("evt= " + evt);
-
-      // TODO: Get the list of followers, then read from users table to get the
-      // the notificationToken for each user. Finally, use FCM to push notification to user.
-
-      return;
+  // For each message check if there was an error.
+  console.log(`FCM successfully sent ${response.successCount} messages.`);
+  const tokensToRemove = [];
+  response.results.forEach((result, index) => {
+    const error = result.error;
+    if (error) {
+      console.error('Failure sending notification to', token, error);
+      // Cleanup the tokens who are not registered anymore.
+      if (error.code === 'messaging/invalid-registration-token' ||
+          error.code === 'messaging/registration-token-not-registered') {
+        tokensToRemove.push(tokensSnapshot.ref.child(tokens).remove());
+      }
+    }
   });
+  return Promise.all(tokensToRemove);
+}
+
+async function getTokenAndNotify(follower, message)
+{
+  const documentPath = `users/${follower}`;
+  const document = db.doc(documentPath);
+  console.log(`Getting token from document path: ${documentPath}`);
+  try {
+    let query = await document.get();
+    if (!query.exists) {
+      console.log(`${follower} does not exist in users.`);
+    }
+    else {
+      let token = query.get("token");
+      console.log("user", follower, "token", token);
+      return sendNotification(token, message);
+    }
+  }
+  catch (err) {
+    console.log('Error getting document', err);
+  }
+}
+
+async function handleTimeUpdate(message, newData)
+{
+  console.log(message);
+  const followers = newData["followers"];
+  for (let follower of followers) {
+    getTokenAndNotify(follower, message);
+  }
+}
+
+// Listen for any change in collection `events`.
+exports.handleEventUpdate = functions.firestore
+  .document('competitions/{competition}/events/{event}').onWrite((change, context) => {
+      console.log("Received change to events collection");
+      const oldData = change.before.data();
+      const newData = change.after.data();
+      console.log(`Old data = ${JSON.stringify(oldData)}`);
+      console.log(`New data = ${JSON.stringify(newData)}`);
+
+      // Check for changes in startTime
+      if (oldData["startTime"].toDate().getTime() !== newData["startTime"].toDate().getTime()) {
+        const newDataStartTime = newData["startTime"].toDate();
+        const message = `The starttime for ${newData["name"]} has been changed to: ${newDataStartTime}`;
+        handleTimeUpdate(message, newData);
+      }
+
+      // Check for changes in endTime
+      if (oldData["endTime"].toDate().getTime() !== newData["endTime"].toDate().getTime()) {
+        const newDataEndTime = newData["endTime"].toDate();
+        const message = `The end time for ${newData["name"]} has been changed to: ${newDataEndTime}`;
+        handleTimeUpdate(message, newData);
+      }
+      return 0;
+    });
